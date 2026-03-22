e50680e2b947380159e36f9899c1e59f956d593a
Nёll <nlxria.gl@gmail.com>
2026-03-22 12:59:11 +0900

    Commit

--- scripts/rules.js
+++ scripts/rules.js
@@ -1,6 +1,6 @@
 var pre = document.querySelector("pre");
 pre.innerHTML = pre.innerHTML.replaceAll(/\|([^|《》]+)《([^|《》]+)》/g,`<ruby ruby="$2">$1</ruby>`);
-document.querySelectorAll("pre :is(h1,h2,h3,span):not([id])").forEach(element=>element.id=element.innerText);
+document.querySelectorAll("pre :is(h1,h2,h3,span:not([id]))").forEach(element=>element.id=element.innerText);
 var newContents = document.querySelector("#新しい要素").parentElement;
 var tableOfContents = document.querySelector("#目次").parentElement;
 document.querySelectorAll("pre :is(h1,h2,h3)").forEach(element=>tableOfContents.innerHTML+="<br>"+(element.tagName=="H1"?`<b>＃${element.id}</b>`:element.tagName=="H2"?`　　＃${element.id}`:`　　　　＃${element.id}`));
