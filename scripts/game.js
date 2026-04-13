6a7380dddb7250d5072f1c3db070d8913be2a8e7
NLXRIA <nlxria.gl@gmail.com>
2026-04-14 01:52:23 +0900

    Commit

--- scripts/game.js
+++ scripts/game.js
@@ -68,7 +68,7 @@ onSnapshot(q, (snapshot) => {
         // メッセージをラッパーで囲み、data-tag を付与する
         messages += `
         <div class="message-item" data-tag="${tag}">
-            <div class="message-header"><b style="color:${data.color};">${safeName}</b><span>/ ${tag == "comment" ? "コメント" : "システム"}</span></div>
+            <div class="message-header"><b style="color:${data.color};">${safeName}</b><span>＠ ${tag == "comment" ? "ユーザー" : "システム"}</span></div>
             <p>${safeContent}</p>
         </div>`;
     });
